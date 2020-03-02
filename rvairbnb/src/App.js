ba24883866da8c5f6de05588f4accfa7f750010b
eoinlynchcodes <eoin95lynch@gmail.com>
2020-03-02 14:29:36 +0000

    Still with issues

--- rvairbnb/src/App.js
+++ rvairbnb/src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React from "react";
+import { NavLink, Route } from "react-router-dom";
+import Register from './components/Register';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-     
+      <NavLink to='/register'>Register</NavLink>
+      <NavLink to='/login'>Login</NavLink>
 
-
+      <Route exact path='/' component={App}/>
+      <Route path='/register' component={Register}/>
+      <Route path='/login' component={Login}/>
     </div>
+
   );
 }
 
