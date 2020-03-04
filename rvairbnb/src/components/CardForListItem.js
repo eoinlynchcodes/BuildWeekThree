2d52cd4f38b970f354348baf6516720bdbcfdf3b
eoinlynchcodes <eoin95lynch@gmail.com>
2020-03-04 18:40:07 +0000

    Some styles added..

--- rvairbnb/src/components/CardForListItem.js
+++ rvairbnb/src/components/CardForListItem.js
@@ -1,15 +1,20 @@
 import React from "react";
+import styled from 'styled-components';
 
 export const CardForListItem = props => {
   console.log(props.item);
 
   const dataForCard = props.item;
-
+  const Card = styled.div`
+  border: 1px solid black;
+  border-radius: 12px;
+  margin: 0 auto;
+  `
   return (
-    <div>
+    <Card>
       <h1>{dataForCard.location}</h1>
       <h1>{dataForCard.description}</h1>
       <h1>{dataForCard.price_per_day}</h1>
-    </div>
+    </Card>
   );
 };
